636397ba082d4d462ef98ab2cf253f27bd980ad7
Moussa KANE <princekan9.mk@gmail.com>
2022-09-11 10:51:03 -0700

    Revert "This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n."
    This reverts commit 9295496d359bfb9e16479749dcf3f6a7e13d28da.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,23 +10,26 @@
 int main(void)
 {
 	int n;
-	int l;
+<<<<<<< HEAD
+	char last[] = "last digit of";
+=======
+>>>>>>> 5d928ba8163e1a20c9f47fdf85fc277d8f8880ea
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l = n % 10;
 
-	if (l > 5)
+	printf("%s %d is %d and is", last, n, n % 10);
+	if (n % 10 > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+		printf("greater than 5\n");
 	}
-	else if (l == 0)
+	else if (n % 10 == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, l);
+		printf("0\n");
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-        }
+		printf("less than 6 ans not 0\n");
+	}
 	return (0);
 }
