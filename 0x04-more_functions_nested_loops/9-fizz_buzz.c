852d44b64f186c84ec5ba9ecf95ec01af4bf11c8
Moussa KANE <princekan9.mk@gmail.com>
2022-09-15 18:05:49 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	int g;
+
+	for (g = 1 ; g < 100 ; g++)
+	{
+		if ((g % 3) == 0 && (g % 5) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (g % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (g % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", g);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}
