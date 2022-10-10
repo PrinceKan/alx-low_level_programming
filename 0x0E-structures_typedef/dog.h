929bc6cce7ebb1c9b970988f621e542361b47ee5
Moussa KANE <princekan9.mk@gmail.com>
2022-10-10 15:51:01 -0700

    Revert "A dog is the only thing on earth that loves you more than you love yourself"
    This reverts commit 61dc3f15dd8ca7f240c49b1945deb436b9236303.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,12 +12,8 @@ struct dog
 {
 	char *name;
 	float age;
-
+	char *owner;
+};
 /*Typedefs*/
 typedef struct dog dog_t;
-/*Prototypes*/
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
