0998cd36232a1390f8db4f04492069f959d4b75d
Moussa KANE <princekan9.mk@gmail.com>
2022-10-10 13:11:46 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the name of the file wich will be compiled
+ *
+ * Return: the name 0 with succes
+ *
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
