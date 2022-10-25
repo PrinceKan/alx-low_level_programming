9323f12afbe73ee8a4780d193438573ae255e3ed
Moussa KANE <princekan9.mk@gmail.com>
2022-10-25 15:25:59 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,8 +43,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
-		newNode->next = move->next;
-		move->next = newNode;
+	newNode->next = move->next;
+	move->next = newNode;
 
 	return (newNode);
 }
