5af724033497a1ae8967b0c9361a37a8311648a1
Moussa KANE <princekan9.mk@gmail.com>
2023-01-06 09:36:26 -0800

    >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table
+ *
+ * Return: void.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int idx;
+	hash_node_t *new_node;
+	char *comma;
+
+	comma = "";
+
+	if (ht != NULL)
+	{
+		printf("{");
+
+		for (idx = 0; idx < ht->size; idx++)
+		{
+			new_node = ht->array[idx];
+
+			while (new_node != NULL)
+			{
+				printf("%s'%s': '%s'", comma, new_node->key, new_node->value);
+				comma = ", ";
+				new_node = new_node->next;
+			}
+		}
+		printf("}\n");
+	}
+	else
+	{
+		return;
+	}
+}
