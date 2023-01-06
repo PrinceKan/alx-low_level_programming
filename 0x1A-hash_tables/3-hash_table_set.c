ead2e746f36c6db83639b9b6193a6f76b189ded7
Moussa KANE <princekan9.mk@gmail.com>
2023-01-06 09:24:30 -0800

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -35,13 +35,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 
 	if (ht->array[idx] == NULL)
-	{
 		new_node->next = NULL;
-	}
 	else
-	{
 		new_node->next = ht->array[idx];
-	}
 	ht->array[idx] = new_node;
 	return (1);
 }
